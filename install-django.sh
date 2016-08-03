24958c6bb9874ee398a7bedec713f737337ad66e
XAVI3R <xavier.rutayisire@gmail.com>
2016-08-03 13:18:28 +0200

    add support if project already exist

--- install-django.sh
+++ install-django.sh
@@ -12,10 +12,25 @@ working_dir_django_virtualenv="/srv/projects/vectortiles/project/osm-ireland/uti
 
 $working_dir_django_virtualenv/bin/pip3 install Django
 
+#  If composite already exist
+if [ -d "$working_dir_django/composite" ]; then
+ while true; do
+   read -p "Porject 'composite' folder already exist in $working_dir_django directory, yes will delete composite folder, no will end the script. Y/N?" yn
+      case $yn in
+        [Yy]* ) rm -rf  "$working_dir_django/composite"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+      esac
+   done
+fi
+
 cd $working_dir_django
 $working_dir_django_virtualenv/bin/django-admin startproject composite
+cd -
+
 cd $working_dir_django/composite
 $working_dir_django_virtualenv/bin/python manage.py startapp map
+cd -
 
 mkdir -p $working_dir_django/composite/map/templates/map \
          $working_dir_django/composite/map/static/map
