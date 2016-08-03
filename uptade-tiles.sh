5d92e3fb5ef1c19ef0b9e4614096a9abe2b8912d
XAVI3R <xavier.rutayisire@gmail.com>
2016-08-03 16:06:10 +0200

    add utilery host diff

--- uptade-tiles.sh
+++ uptade-tiles.sh
@@ -29,6 +29,9 @@ min_zoom_diff=0
 #  Max zoom tiles
 max_zoom_diff=14
 
+#  Utilery host
+utilery_host_diff="localhost"
+
 ####  END SETUP USER  ####
 
-sh ./utilery/tiles-diff.sh $working_dir_diff $working_dir_diff_virtualenv $working_dir_diff_tiles $database_user_diff $database_user_password_diff $database_name_diff $database_host_diff $min_zoom_diff $max_zoom_diff
+sh ./utilery/tiles-diff.sh $working_dir_diff $working_dir_diff_virtualenv $working_dir_diff_tiles $database_user_diff $database_user_password_diff $database_name_diff $database_host_diff $min_zoom_diff $max_zoom_diff $utilery_host_diff
