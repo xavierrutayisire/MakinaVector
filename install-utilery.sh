2345da6425b8f478969469c2442f337cf69fbfd9
XAVI3R <xavier.rutayisire@gmail.com>
2016-08-18 15:37:20 +0200

    change install of utilery

--- install-utilery.sh
+++ install-utilery.sh
@@ -40,11 +40,91 @@ while true; do
      esac
 done
 
+#  If utilery already exist
+if [ -d "$working_dir_utilery/utilery" ]; then
+ while true; do
+   read -p "Utilery folder already exist in $working_dir_utilery directory, yes will delete utilery folder, no will end the script. Y/N?" yn
+      case $yn in
+        [Yy]* ) rm -rf  "$working_dir_utilery/utilery"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+      esac
+   done
+fi
+
 #### Configuration ####
+
+#  Update of the repositories and install of python, pip, virtualenv, virtualenvwrapper git and libpq-dev
+apt-get update && \
+apt-get install -y python$python_version_utilery python$python_version_utilery-dev python3-pip python-virtualenv virtualenvwrapper git libpq-dev gdal-bin nodejs npm
+
+npm install http-server -g
+ln -s /usr/bin/nodejs /usr/bin/node
+
+mkdir -p $working_dir_utilery
 
 #  Database port (default: 5432)
 database_port_utilery="5432"
 
 #### Installation of Utilery ####
 
-sh ./utilery/utilery.sh $working_dir_utilery $database_user_utilery $database_user_password_utilery $database_name_utilery $database_host_utilery
+#  Clone utilery
+cd $working_dir_utilery
+git clone https://github.com/etalab/utilery
+cd -
+
+#  Add the support of multicores
+rm $working_dir_utilery/utilery/utilery/serve.py
+cat > $working_dir_utilery/utilery/utilery/serve.py << EOF1
+from utilery.views import app
+from werkzeug.serving import run_simple
+from multiprocessing import cpu_count
+
+run_simple('0.0.0.0', 3579, app, use_debugger=True, use_reloader=True, processes=cpu_count())
+EOF1
+
+#  Change the database connexion
+rm $working_dir_utilery/utilery/utilery/config/default.py
+cat > $working_dir_utilery/utilery/utilery/config/default.py << EOF1
+DATABASES = {
+    "default": "dbname=$database_name_utilery user=$database_user_utilery password=$database_user_password_utilery host=$database_host_utilery"
+}
+RECIPES = ['$queries_path_utilery/utilery/queries.yml']
+TILEJSON = {
+    "tilejson": "2.1.0",
+    "name": "utilery",
+    "description": "A lite vector tile server",
+    "scheme": "xyz",
+    "format": "pbf",
+    "tiles": [
+        "http://vector.myserver.org/all/{z}/{x}/{y}.pbf"
+    ],
+}
+BUILTIN_PLUGINS = ['utilery.plugins.builtins.CORS']
+PLUGINS = []
+DEBUG = False
+SRID = 900913
+SCALE = 1
+BUFFER = 0
+CLIP = True
+CORS = "*"
+EOF1
+
+# Import of the queries you wanna use
+cp ./utilery/queries.yml $working_dir_utilery/utilery
+cp ./utilery/new-querie.yml $working_dir_utilery/utilery
+
+#  Create the virtualenv
+rm -rf $working_dir_utilery/utilery-virtualenv
+cd $working_dir_utilery
+virtualenv utilery-virtualenv --python=/usr/bin/python$python_version_utilery
+cd -
+
+#  Install python package
+cd $working_dir_utilery/utilery
+$working_dir_utilery/utilery-virtualenv/bin/pip3 install --upgrade pip
+$working_dir_utilery/utilery-virtualenv/bin/pip3 install .
+cd -
+
+#  Install unstable python dependencies
+$working_dir_utilery/utilery-virtualenv/bin/pip3 install -r $working_dir_utilery/utilery/requirements.txt
