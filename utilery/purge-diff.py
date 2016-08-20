df08725328bdeffa4c9642d32e3176a7c6a83de6
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2016-08-20 05:56:36 +0200

    Update purge-diff.py

--- utilery/purge-diff.py
+++ utilery/purge-diff.py
@@ -10,55 +10,63 @@ from subprocess import Popen
 import os
 from http.client import HTTPConnection
 
+# Database connection
 conn = psycopg2.connect(host=sys.argv[5], database=sys.argv[4], user=sys.argv[2], password=sys.argv[3])
 cursor = conn.cursor()
+
+# Request to get all the diff not processed 
 cursor.execute("SELECT * FROM diff where processed = false")
 records = cursor.fetchall()
+
 tiles_generate = []
 
 for record in records:
+    # Request to the geometry
     to_generate = conn.cursor()
     to_generate.execute("SELECT ST_AsGeoJSON(ST_Transform(geometry, 4326), 15, 1) as geojson FROM diff where id = %s" % (record[0]))
     point = to_generate.fetchall()
     bbox = json.loads(point[0][0])
-
+    
+    # Zoom
     minzoom = int(sys.argv[6])
     maxzoom = int(sys.argv[7])
+    
+    # Zone
     west = bbox["bbox"][0]
     south = bbox["bbox"][1]
     east = bbox["bbox"][2]
     north = bbox["bbox"][3]
+    
+    # Utilery host (by varnish)
     host = sys.argv[8]
     port = sys.argv[9]
-    directory_generation = sys.argv[1]
-    procs = []
     conn = HTTPConnection(host:port) 
-
+    
+    # Variable to prevent stack overflow
+    procs = []
+    
     for zoom in range(minzoom, maxzoom + 1):
-        if not os.path.exists("%s/%s" % (directory_generation, zoom)):
-            os.makedirs("%s/%s" % (directory_generation, zoom))
         west_south_tile = mercantile.tile(west, south, zoom)
         east_north_tile = mercantile.tile(east, north, zoom)
         for x in range(west_south_tile.x, east_north_tile.x + 1):
-            if not os.path.exists("%s/%s/%s" % (directory_generation, zoom, x)):
-                os.makedirs("%s/%s/%s" % (directory_generation, zoom, x))
             for y in range(east_north_tile.y, west_south_tile.y + 1):
-                filename = "%s/%s/%s" % (directory_generation, zoom, x)
                 tile_already_generate = 0
-                if not os.path.isfile(filename):
-                    url = "/all/%s/%s/%s.pbf" % (zoom, x, y)
-                    for tile in tiles_generate:
-                        if(tile == url):
-                            tile_already_generate = 1
-                            break
-                    if(tile_already_generate == 0):
-                        print(zoom, x, y)                 
-                        tiles_generate.append(url)
-                        procs.append(conn.request("PURGE", url))
-                        if len(procs) > (cpu_count() * 4):
-                            procs[0].wait()
-                            procs.remove(procs[0])
+                url = "/all/%s/%s/%s.pbf" % (zoom, x, y)
+                for tile in tiles_generate:
+                    if(tile == url):
+                        tile_already_generate = 1
+                        break
+                if(tile_already_generate == 0):
+                    print(zoom, x, y)                 
+                    tiles_generate.append(url)
+                    procs.append(conn.request("PURGE", url))
+                    # To prevent stack overflow
+                    if len(procs) > (cpu_count() * 4):
+                        procs[0].wait()
+                        procs.remove(procs[0])
+    # Set the diff as processed
     update_diff = conn.cursor()
     update_diff.execute("UPDATE diff SET processed = true WHERE id = %s" % (record[0]))
 
+# Save the change
 conn.commit()
