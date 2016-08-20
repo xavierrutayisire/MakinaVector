50e075906a202c85eec3878b2b8713136b6f87a2
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2016-08-20 05:20:24 +0200

    Update cache-tiles.py
    Only cache support

--- cache-tiles.py
+++ cache-tiles.py
@@ -1,11 +1,16 @@
 # SETUP USER
 
+# Zoom to cache
 minzoom = 0
 maxzoom = 14
+
+# Delimited zone to cache
 west = -11.1133787
 south = 51.122
 east = -5.5582362
 north = 55.736
+
+# Utilery connection (by Varnish)
 host = '127.0.0.1'
 port = 6081
 
@@ -17,26 +22,19 @@ from multiprocessing import cpu_count
 from subprocess import Popen 
 import os
 
+# Variable to prevent stack overflow
 procs = []
-directory_generation = '/dev/null'
 
-if not os.path.exists("%s" % (directory_generation)):
-		os.makedirs("%s" % (directory_generation))
-
+# Cache all the tiles in the delimited zone
 for zoom in range(minzoom, maxzoom + 1):
-	if not os.path.exists("%s/%s" % (directory_generation, zoom)):
-		os.makedirs("%s/%s" % (directory_generation, zoom))
 	west_south_tile = mercantile.tile(west, south, zoom)
 	east_north_tile = mercantile.tile(east, north, zoom)
 	for x in range(west_south_tile.x, east_north_tile.x + 1):
-		if not os.path.exists("%s/%s/%s" % (directory_generation, zoom, x)):
-			os.makedirs("%s/%s/%s" % (directory_generation, zoom, x))
 		for y in range(east_north_tile.y, west_south_tile.y + 1):
 			print(zoom, x, y)
-			filename = "%s/%s/%s" % (directory_generation, zoom, x)
-			if not os.path.isfile(filename):
-				url = "http://%s:%s/default/all/%s/%s/%s.pbf" % (host, port, zoom, x, y)
-				procs.append(subprocess.Popen(['wget', url, '-P', filename, '-q']))
-				if len(procs) > (cpu_count() * 4):
-					procs[0].wait()
-					procs.remove(procs[0]) 
+			url = "http://%s:%s/all/%s/%s/%s.pbf" % (host, port, zoom, x, y)
+			procs.append(subprocess.Popen(['curl', url, '>', '/dev/null', '2>&1']))
+			# To prevent stack overflow
+			if len(procs) > (cpu_count() * 4): 
+				procs[0].wait()
+				procs.remove(procs[0]) 
