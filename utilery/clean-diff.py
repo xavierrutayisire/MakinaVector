dfec69a9a88a2daa1af9948364eaa7a939f9619a
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2016-08-20 05:48:41 +0200

    Update clean-diff.py

--- utilery/clean-diff.py
+++ utilery/clean-diff.py
@@ -2,7 +2,10 @@
 import psycopg2
 import sys
 
+# Database connection
 conn = psycopg2.connect(host=sys.argv[4], database=sys.argv[3], user=sys.argv[1], password=sys.argv[2])
 cursor = conn.cursor()
+
+# Request
 cursor.execute("DELETE FROM diff WHERE processed = true")
 conn.commit()
