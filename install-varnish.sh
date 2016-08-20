9698b000134111648e04e1d2bb800cb701bfc35e
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2016-08-20 23:02:16 +0200

    Update install-varnish.sh

--- install-varnish.sh
+++ install-varnish.sh
@@ -95,7 +95,6 @@ curl https://repo.varnish-cache.org/GPG-key.txt | apt-key add -
 echo "deb https://repo.varnish-cache.org/ubuntu/ trusty varnish-4.0" \
      >> /etc/apt/sources.list.d/varnish-cache.list
 apt-get update && \
-apt-get upgrade -y && \
 apt-get install -y varnish
 
 #  Creation of vcl varnish service
